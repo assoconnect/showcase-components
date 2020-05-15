d4fd5b1588d800fd00e730b882ce653fd70c1397
Axel Gaillard <axel.gaillard@assoconnect.com>
2020-05-15 16:55:21 +0200

    Revert "BUGFIX : Axel 16778"
    This reverts commit dec436c523ea1d59fd5d6f003d4cc3cf63c8987a.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -64,7 +64,6 @@ class Layout extends Component {
         },
       },
       translations,
-      homeUrl
     } = this.props
     const url = typeof window !== 'undefined' ? window.location.href : ''
     return (
@@ -123,7 +122,6 @@ class Layout extends Component {
               cta={cta}
               items={headerItems}
               translations={translations}
-              homeUrl={homeUrl}
             />
             {children}
             <Footer

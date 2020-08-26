3c98ef3ff7c0583516a4ec6a43921494b5663fd1
Axel Gaillard <axel.gaillard@assoconnect.com>
2020-08-26 15:22:49 +0200

    BUGFIX Axel 17673

--- src/components/Navigation/NavigationButtons.jsx
+++ src/components/Navigation/NavigationButtons.jsx
@@ -31,6 +31,12 @@ const NavigationButtonsWrapper = styled.div`
 const NavigationButtonsStyled = styled((props) => <ButtonLink {...props} />)`
   display: inline-block;
   margin: 0 10px;
+  
+  & .button--size-small {
+    padding-top: 0.375rem;
+    padding-bottom: 0.375rem;
+  }
+
 
   /* Mobile mode */
   @media screen and (max-width: ${UI.breakpoints.mobile}) {

a20231260b9148fdc70b9e491e3763f6a65635da
Rémi Jean <remijean25@gmail.com>
2021-03-25 17:16:24 +0100

    Fix Roboto 900

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -109,7 +109,7 @@ class Layout extends Component {
           <meta property="og:image:height" content="261" />
 
           <link
-            href="https://fonts.googleapis.com/css?family=Baloo|Caveat:400,700|Roboto:300,400,700&display=swap"
+            href="https://fonts.googleapis.com/css?family=Baloo|Caveat:400,700|Roboto:300,400,700,900&display=swap"
             rel="stylesheet"
           />
         </Helmet>

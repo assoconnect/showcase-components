4e38757d5c1d2b4c1304d3f709c61018dc5f54e5
Rémi Jean <remijean25@gmail.com>
2021-03-31 15:40:28 +0200

    Fix breadcrumbs	margin

--- src/components/Blog/Reusables/BreadCrumbs.js
+++ src/components/Blog/Reusables/BreadCrumbs.js
@@ -14,7 +14,7 @@ const BreadCrumbsWrap = styled.ol`
 
 const BreadCrumbsItem = styled.li`
   margin: 15px 0;
-  padding: 0 5px 0 10px;
+  padding: 0 0 0 15px;
 `
 const BreadCrumbsLink = styled(GatsbyLink)`
   margin: 15px 0;

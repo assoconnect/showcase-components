8bf814d54bbb3381db7b600483b99583e6502e13
Rémi Jean <remijean25@gmail.com>
2021-04-08 15:17:54 +0200

    Fix

--- src/styles/index.js
+++ src/styles/index.js
@@ -4,7 +4,6 @@ import colors from './colors'
 import fonts from './fonts'
 import shadows from './shadows'
 import { createGlobalStyle } from 'styled-components'
-import './fonts/stylesheet.css'
 
 const GlobalStyle = createGlobalStyle`
   /* Reset css */

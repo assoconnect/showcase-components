31fc976155206b4cec35b00bea6074791672fc15
Rémi Jean <remijean25@gmail.com>
2021-05-04 16:05:16 +0200

    Test fix

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -142,31 +142,30 @@ class Layout extends Component {
           {richSnippets && (
             <script type="application/ld+json">{richSnippets}</script>
           )}
+
+          {tagManagerId && (
+            <script>
+              {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${tagManagerId}');
+              `}
+            </script>
+          )}
         </Helmet>
         {tagManagerId && (
-          <>
-            <Helmet defer={true}>
-              <script>
-                {`
-                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
-                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                  })(window,document,'script','dataLayer','${tagManagerId}');
-                `}
-              </script>
-            </Helmet>
-            <noscript>
-              {`
-                <iframe
-                  src="https://www.googletagmanager.com/ns.html?id=${tagManagerId}"
-                  height="0"
-                  width="0"
-                  style="display:none;visibility:hidden"
-                ></iframe>
-              `}
-            </noscript>
-          </>
+          <noscript>
+            {`
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=${tagManagerId}"
+                height="0"
+                width="0"
+                style="display:none;visibility:hidden"
+              ></iframe>
+            `}
+          </noscript>
         )}
 
         <UI.GlobalStyle />

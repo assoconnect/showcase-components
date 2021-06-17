020f215fabd7e14aa9663714281ad09aed5cfac1
Rémi Jean <remijean25@gmail.com>
2021-06-17 18:41:52 +0200

    Add RudderStack condition

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -155,9 +155,10 @@ class Layout extends Component {
               // Loading trackers 
               window._axcb.push(function(axeptio) {
                 axeptio.on("cookies:complete", function(choices) {
-                  console.log(choices);
                   // Load RudderStack
-                  rudderanalytics=window.rudderanalytics=[];for(var methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],i=0;i<methods.length;i++){var method=methods[i];rudderanalytics[method]=function(a){return function(){rudderanalytics.push([a].concat(Array.prototype.slice.call(arguments)))}}(method)}rudderanalytics.load("${rudderStack.key}","${rudderStack.host}"),rudderanalytics.page();
+                  if(choices?.rudderstack) {
+                    rudderanalytics=window.rudderanalytics=[];for(var methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],i=0;i<methods.length;i++){var method=methods[i];rudderanalytics[method]=function(a){return function(){rudderanalytics.push([a].concat(Array.prototype.slice.call(arguments)))}}(method)}rudderanalytics.load("${rudderStack.key}","${rudderStack.host}"),rudderanalytics.page();
+                  }
                 })
               })
             `}

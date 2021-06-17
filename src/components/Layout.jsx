34e23b94e1d04a7b15a8b37e297b9ea35577af80
Rémi Jean <remijean25@gmail.com>
2021-06-17 18:20:49 +0200

    Fix Axeptio

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -74,6 +74,7 @@ class Layout extends Component {
       homePath,
       lang = 'fr',
       richSnippets,
+      tagManagerId,
       axeptio: { clientId, cookiesVersion },
     } = this.props
 
@@ -131,18 +132,37 @@ class Layout extends Component {
 
           <script>
             {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${tagManagerId}');
+            `}
+          </script>
+          <script>
+            {`
               window.axeptioSettings = {
                 clientId: "${clientId}",
                 cookiesVersion: "${cookiesVersion}",
               };
               (function(d, s) {
                 var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
-                e.async = true; e.src = "//static.axept.io/sdk.js";
+                e.async = true; e.src = "//static.axept.io/sdk-slim.js";
                 t.parentNode.insertBefore(e, t);
               })(document, "script");
-          `}
+            `}
           </script>
         </Helmet>
+        <noscript>
+          {`
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=${tagManagerId}"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          `}
+        </noscript>
 
         <UI.GlobalStyle />
         <ThemeProvider

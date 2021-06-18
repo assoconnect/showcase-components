f4240893825fc5e4fabcd8505068e3ca6107f7a2
Rémi Jean <remijean25@gmail.com>
2021-06-18 14:57:57 +0200

    Test

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -156,7 +156,9 @@ class Layout extends Component {
               window._axcb.push(function(axeptio) {
                 axeptio.on("cookies:complete", function(choices) {
                   // Load RudderStack
-                  if(choices?.rudderstack) {
+                  console.log(choices);
+                  console.log(choices.rudderstack);
+                  if(choices.rudderstack) {
                     rudderanalytics=window.rudderanalytics=[];for(var methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],i=0;i<methods.length;i++){var method=methods[i];rudderanalytics[method]=function(a){return function(){rudderanalytics.push([a].concat(Array.prototype.slice.call(arguments)))}}(method)}rudderanalytics.load("${rudderStack.key}","${rudderStack.host}"),rudderanalytics.page();
                   }
                 })

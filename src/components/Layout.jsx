1a78e9b3cce76ae312e237168b9175675b48b11f
RYAN <ryanchoukri@gmail.com>
2021-06-28 10:39:40 +0200

    Revert "Axeptio dev env"
    This reverts commit a29db3dddf1312e02d370b6d85d49f84dae559d4.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -131,60 +131,53 @@ class Layout extends Component {
             <script type="application/ld+json">{richSnippets}</script>
           )}
 
-          {tagManagerId && (
-            <script>
-              {`
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${tagManagerId}');
-              `}
-            </script>
-          )}
-          {axeptio.clientId && axeptio.cookiesVersion && (
-            <script>
-              {`
-                window.axeptioSettings = {
-                  clientId: "${axeptio.clientId}",
-                  cookiesVersion: "${axeptio.cookiesVersion}",
-                };
-                (function(d, s) {
-                  var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
-                  e.async = true; e.src = "//static.axept.io/sdk-slim.js";
-                  t.parentNode.insertBefore(e, t);
-                })(document, "script");
-                void 0 === window._axcb && (window._axcb = []);
-                // Loading trackers 
-                window._axcb.push(function(axeptio) {
-                  axeptio.on("cookies:complete", function(choices) {
-                    // Load RudderStack
-                    if(${!!rudderStack.key} && ${!!rudderStack.host} && choices.rudderstack) {
-                      var script = document.createElement('script');
-                      script.setAttribute('src', 'https://cdn.rudderlabs.com/v1/rudder-analytics.min.js');
-                      document.head.append(script);
-                      rudderanalytics=window.rudderanalytics=[];for(var methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],i=0;i<methods.length;i++){var method=methods[i];rudderanalytics[method]=function(a){return function(){rudderanalytics.push([a].concat(Array.prototype.slice.call(arguments)))}}(method)}rudderanalytics.load("${
-                        rudderStack.key
-                      }","${rudderStack.host}"),rudderanalytics.page();
-                    }
-                  })
-                })
-              `}
-            </script>
-          )}
-        </Helmet>
-        {tagManagerId && (
-          <noscript>
+          <script>
             {`
-              <iframe
-                src="https://www.googletagmanager.com/ns.html?id=${tagManagerId}"
-                height="0"
-                width="0"
-                style="display:none;visibility:hidden"
-              ></iframe>
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.defer=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${tagManagerId}');
             `}
-          </noscript>
-        )}
+          </script>
+          <script>
+            {`
+              window.axeptioSettings = {
+                clientId: "${axeptio.clientId}",
+                cookiesVersion: "${axeptio.cookiesVersion}",
+              };
+              (function(d, s) {
+                var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+                e.async = true; e.src = "//static.axept.io/sdk-slim.js";
+                t.parentNode.insertBefore(e, t);
+              })(document, "script");
+              void 0 === window._axcb && (window._axcb = []);
+              // Loading trackers 
+              window._axcb.push(function(axeptio) {
+                axeptio.on("cookies:complete", function(choices) {
+                  // Load RudderStack
+                  if(choices.rudderstack) {
+                    var script = document.createElement('script');
+                    script.setAttribute('src', 'https://cdn.rudderlabs.com/v1/rudder-analytics.min.js');
+                    document.head.append(script);
+                    rudderanalytics=window.rudderanalytics=[];for(var methods=["load","page","track","identify","alias","group","ready","reset","getAnonymousId","setAnonymousId"],i=0;i<methods.length;i++){var method=methods[i];rudderanalytics[method]=function(a){return function(){rudderanalytics.push([a].concat(Array.prototype.slice.call(arguments)))}}(method)}rudderanalytics.load("${rudderStack.key}","${rudderStack.host}"),rudderanalytics.page();
+                  }
+                })
+              })
+            `}
+          </script>
+        </Helmet>
+        <noscript>
+          {`
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=${tagManagerId}"
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          `}
+        </noscript>
+
         <UI.GlobalStyle />
         <ThemeProvider
           theme={{
